0e7fd8bb01d7ac91b882963e35b414d249f33fe5
willtern <willtern@pop-os.localdomain>
2021-08-19 20:00:01 +0300

    fix:CSS :element(height), element__text(font), element__description(block-name), popup__close-icon(position), popup__container(position), profile__job(font), profile__name(font), profile__edit-button-icon(remove).JS : remove like var, remove like function, commit Esc function and background close function , rename function, closepopup function rewrite, function submitForm change, remove listener for like button. HTML : change png img to svg, add h2 element__description...

--- js/script.js
+++ js/script.js
@@ -6,74 +6,45 @@ let nameInput = formElement.querySelector('.popup__input_element_name');
 let jobInput = formElement.querySelector('.popup__input_element_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let likebutton = document.querySelectorAll('.element__button')[0];
-let likebutton2 = document.querySelectorAll('.element__button')[1];
-let likebutton3 = document.querySelectorAll('.element__button')[2];
-let likebutton4 = document.querySelectorAll('.element__button')[3];
-let likebutton5 = document.querySelectorAll('.element__button')[4];
-let likebutton6 = document.querySelectorAll('.element__button')[5];
+let textWarp = document.querySelectorAll('.element__text')[5];
 
-function toggleLike() {
-  likebutton.classList.toggle('element__button-image_active');
-}
-
-function toggleLike2() {
-  likebutton2.classList.toggle('element__button-image_active');
-}
-
-function toggleLike3() {
-  likebutton3.classList.toggle('element__button-image_active');
-}
-
-function toggleLike4() {
-  likebutton4.classList.toggle('element__button-image_active');
-}
-
-function toggleLike5() {
-  likebutton5.classList.toggle('element__button-image_active');
-}
-
-function toggleLike6() {
-  likebutton6.classList.toggle('element__button-image_active');
-}
-
-document.addEventListener ('keydown', function(event) {
-  if(event.key === 'Escape') {
-    popup.classList.remove('popup_opened');
-    }
-  })
-
-  document.querySelector('.popup__container').addEventListener(
-    'click', function (event) {
-      event.stopPropagation();
-    }
-  )
-
-function popupOpen(){
+//Добавление стилей для последнего .element__text
+textWarp.setAttribute('style', `text-overflow: ellipsis;
+white-space: nowrap;
+overflow: hidden;`)
+// Закртыие попы на ESC
+//document.addEventListener ('keydown', function(event) {
+//  if(event.key === 'Escape') {
+//    popup.classList.remove('popup_opened');
+//  }
+//})
+//Закрытие попы по клику фона
+//  document.querySelector('.popup__container').addEventListener(
+//    'click', function (event) {
+//      event.stopPropagation();
+//    }
+//  )
+//Функция открытия попы
+function openPopup(){
   popup.classList.add('popup_opened');
-  nameInput.textContent = profileName.textContent;
-  jobInput.textContent = profileJob.textContent;
-}
-function popupClose(){
-  popup.classList.remove('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
-function formSubmit (event) {
-  event.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
+
+//Функция закрытия попы
+function closePopup(){
   popup.classList.remove('popup_opened');
 }
 
-popup.addEventListener('click', popupClose);
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-formElement.addEventListener('submit', formSubmit);
-likebutton.addEventListener('click', toggleLike);
-likebutton2.addEventListener('click', toggleLike2);
-likebutton3.addEventListener('click', toggleLike3);
-likebutton4.addEventListener('click', toggleLike4);
-likebutton5.addEventListener('click', toggleLike5);
-likebutton6.addEventListener('click', toggleLike6);
+//Функция сохранения и отправки значений в профиль
+function submitForm (event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
 
+//popup.addEventListener('click', closePopup);// слушатель закрытия попы по фону
+editButton.addEventListener('click', openPopup); //слушатель открытия попы по клику на кнопку
+closeButton.addEventListener('click', closePopup);//слушатель закрытия попы по клику на копку
+formElement.addEventListener('submit', submitForm);//слушатель отправки значений
